6b86ba92c004c32330338c70f801bcae4fd2477b
Ignat Anikovich <tanokina86@gmail.com>
2019-02-25 15:24:31 +0300

    add some code

---
+++ ConsoleAccess/Access.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAccess
+{
+    public class Access
+    {
+        int a; // все равно, что private int a;
+        private int b; // поле доступно только из текущего класса
+        protected int c; // доступно из текущего класса и производных классов
+        internal int d; // доступно в любом месте программы
+        protected internal int e; // доступно в любом месте программы и из классов-наследников
+        public int f; // доступно в любом месте программы, а также для других программ и сборок            
+
+        public void DisplayPrivate(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            Console.WriteLine($"private variable \"a\" = {a}");
+            Console.WriteLine($"private variable \"b\" = {b}");
+        }
+
+        protected void DisplayInternal()
+        {
+            Console.WriteLine($"internal variable \"e\" = {e}");
+        }
+        private void DisplayPublic()
+        {
+            Console.WriteLine($"public variable \"f\" = {f}");
+        }
+    }
+}
---
+++ ConsoleAccess/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAccess
+{
+    class Program:Access
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            Program program = new Program();
+            //only inherit classes can use protected members
+            //только классы-наслденики могут использовать защищенные члены класса(protected)
+            program.DisplayPrivate(random.Next(0, 6), random.Next(0, 6));
+            Console.WriteLine($"protected variable \"c\" = {program.c = random.Next(0, 6)}");
+            Console.WriteLine($"private variable \"d\" = {program.d = random.Next(0, 6)}");
+            program.DisplayInternal();
+            program.f = 20;//мы не может напрямую обратиться к методу DisplayPublic(), т.к он закрыт
+            //Но открыта переменная f и мы используем её
+            Console.WriteLine($"public variable \"f\" = {program.f}");
+            Console.ReadKey();
+        }
+    }
+}
